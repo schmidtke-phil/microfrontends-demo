06714f9be946ebec41090851820aeca6752b532a
schmidtke-phil <phil.schmidtke@telekom.de>
2022-09-28 17:01:19 +0200

    Init

---
+++ mf-svelte-1/src/App.svelte
@@ -0,0 +1,6 @@
+<div class="mt-10 text-3xl mx-auto max-w-6xl">
+  <div>Name: mf-svelte-1</div>
+  <div>Framework: svelte</div>
+  <div>Language: TypeScript</div>
+  <div>CSS: Tailwind</div>
+</div>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Microfrontend Demo using Webpack Module Federation
+
+## Demo with a React host, react mf's, vue mf and a svelte mf
